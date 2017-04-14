d67355405ea101eefb0d141525cfe994c6b37ac7
Janet Jie Deng <janetjied@gmail.com>
2017-04-14 14:21:01 +0200

    First commit, WhiteNoise & ParameterEstimation.

---
+++ ParameterEstimation.m
@@ -0,0 +1,19 @@
+clear;
+model = arima('Constant', 0, 'AR', {1.3, -0.65}, 'Variance', 280);
+
+m = 500;
+rng('default')
+Y = simulate(model, m);
+
+% d is the estimated model parameter from Yule-Walker estimation
+% p error variance
+[d, p] = aryule(Y,2);
+
+figure(1)
+plot(Y)
+xlim([0, m])
+title('Simulated AR(2) Process')
+
+figure(2)
+autocorr(Y, 50)
+grid on;
