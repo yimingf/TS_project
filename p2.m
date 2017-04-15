5a720efa54dca2d0a3d1137443867c1dae83f2a7
Fan Yiming <alkalidic@gmail.com>
2017-04-15 14:08:11 +0200

    finished p2

--- p2.m
+++ p2.m
@@ -3,19 +3,28 @@
 clear;
 model = arima('Constant', 0, 'AR', {1.3, -0.65}, 'Variance', 280);
 
-m = 500;
-rng('default')
-Y = simulate(model, m);
+m = 50; % number of samples.
+n = 500; % size of data.
+rng('default');
+Y = cell(1, m);
+param_mle = zeros(2, m);
+param_yule = zeros(2, m);
+for i=1:m
+  Y{i} = simulate(model, n); % generate data
 
-% d is the estimated model parameter from Yule-Walker estimation
-% p error variance
-[d, p] = aryule(Y,2);
+  mle = arma_mle(Y{i}, 2, 0);
+  param_mle(:, i) = mle.ar; % estimate by ml
+
+  [d, ~] = aryule(Y{i}, 2);
+  param_yule(:, i) = d(2:3)';
+end
 
 figure(1)
-plot(Y)
-xlim([0, m])
-title('Simulated AR(2) Process')
+scatter(param_mle(1, :), param_mle(2, :));
+title('Scatter plot of ML estimation');
+grid on;
 
 figure(2)
-autocorr(Y, 50)
+scatter(param_yule(1, :), param_yule(2, :));
+title('Scatter plot of Yule-Walker estimation');
 grid on;
--- readme.md
+++ readme.md
@@ -6,8 +6,15 @@ p1
 - tried ljung-box q-test.
 - deviation in AR(1) could be tested on both validators, but failed in AR(3)
 
+p2
+---
+
+- tried ML and YW methods.
+- scatter plots are made.
+
 TODO
 ---
 
 - try another testing method in p1
 - test the coefficients and the effect of sample size.
+- accuracy / prediction error / histogram / residual
