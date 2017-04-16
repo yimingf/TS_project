c2dc01b767c52c8e55be6c5b7f48e37eb75a3553
Fan Yiming <alkalidic@gmail.com>
2017-04-16 14:43:57 +0200

    completed p1

--- p1.m
+++ p1.m
@@ -2,26 +2,33 @@
 
 clear;
 % 1. generate iid ts datas.
-n = 500; % size n
+n = 100000; % size n
 mu = 0;
 sigma = 1;
 h = 4; % lag
-y = sigma*randn(n, 1)+mu; % simulate iid samples
-rho = autocorr(y, h);
-pacf = parcorr(y);
+% y = sigma*randn(n, 1)+mu; % simulate iid samples
+% rho = autocorr(y, h);
+% pacf = parcorr(y);
 
 % 2. different tests.
 % (a) test the claim that p(h) has gaussian distribution with mean zero and variance 1/n.
+% var = 1/n;
+% h_1 = kstest(rho/sqrt(var));
+
+% % (b) the portmanteau test (ljung-box q-test).
+% h_2 = lbqtest(rho-mean(y));
+
+% 3. test with different models such as AR and MA.
+ar = arima('Constant',0.05,'MA',{0.7},'Variance',0.01); % AR(3) model.
+y = simulate(ar, n);
+rho = autocorr(y, h);
+
+% (a) test the claim that p(h) has gaussian dist w/ mean 0 and variance 1/n.
 var = 1/n;
 h_1 = kstest(rho/sqrt(var));
 
 % (b) the portmanteau test (ljung-box q-test).
 h_2 = lbqtest(rho-mean(y));
-
-% 3. test with different models such as AR and MA.
-ar = arima('Constant',0.05,'AR',{0.9},'Variance',0.01); % AR(3) model.
-y = simulate(ar, n);
-rho = autocorr(y, h);
 
 % figure(1)
 % x = 1:1:n;
@@ -31,7 +38,7 @@ rho = autocorr(y, h);
 % ylabel('Sample Values');
 % grid on;
 
-% figure(2)
-% x = 1:1:n;
-% autocorr(y)
-% grid on;
+figure(2)
+x = 1:1:n;
+autocorr(y)
+grid on;
