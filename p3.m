4b13e6d159ba35f3c39889adc2e841936eefac44
Fan Yiming <alkalidic@gmail.com>
2017-04-18 18:34:29 +0200

    well

--- p3.m
+++ p3.m
@@ -2,35 +2,35 @@
 
 clc, clear;
 % a) Predictions on \textbf{AR(2)} model.
-% model = arima('Constant', 0, 'AR', {1.3, -0.65}, 'Variance', 280);
+model = arima('Constant', 0, 'AR', {1.3, -0.65}, 'Variance', 280);
 
-% m = 50;
-% n = 300;
+m = 50;
+n = 300;
 
-% Y = zeros(n, m);
-% param_2 = zeros(2, m);
-% param_10 = zeros(10, m); % AR(10)
+Y = zeros(n, m);
+param_2 = zeros(2, m);
+param_10 = zeros(10, m); % AR(10)
 
-% for i=1:m
-%   Y(:, i) = simulate(model, n);
+for i=1:m
+  Y(:, i) = simulate(model, n);
 
-%   mle = arma_mle(Y(:, i), 2, 0);
-%   param_2(:, i) = mle.ar;
+  mle = arma_mle(Y(:, i), 2, 0);
+  param_2(:, i) = mle.ar;
 
-%   mle = arma_mle(Y(:, i), 10, 0);
-%   param_10(:, i) = mle.ar;
-% end
+  mle = arma_mle(Y(:, i), 10, 0);
+  param_10(:, i) = mle.ar;
+end
 
-% predict_2   = zeros(n, m);
-% predict_10  = zeros(n, m);
-% K = 3;
-% for i=1:m
-%   predict_2 (:, i) = prediction(param_2(:, i), Y(:, i), K);
-%   predict_10(:, i) = prediction(param_10(:, i), Y(:, i), K);
-% end
+predict_2   = zeros(n, m);
+predict_10  = zeros(n, m);
+K = 3;
+for i=1:m
+  predict_2 (:, i) = prediction(param_2(:, i), Y(:, i), K, 0);
+  predict_10(:, i) = prediction(param_10(:, i), Y(:, i), K, 0);
+end
 
-% error_2 =mean(mean((Y(1+K:n, :)-predict_2 (1+K:n, :)).^2))
-% error_10=mean(mean((Y(1+K:n, :)-predict_10(1+K:n, :)).^2))
+error_2 =mean(mean((Y(1+K:n, :)-predict_2 (1+K:n, :)).^2))
+error_10=mean(mean((Y(1+K:n, :)-predict_10(1+K:n, :)).^2))
 
 % % b) Prediction on \textbf{AR(1)} model.
 % model = arima('Constant', 0, 'AR', {0.8}, 'Variance', 1);
@@ -57,7 +57,7 @@ clc, clear;
 % K = 3;
 % for i=1:m
 %   predict_ar(:, i) = prediction(param_ar(:, i), Y(:, i), K);
-%   model = arima('Constant', 0, 'MA', param_ma(:, i), 'Variance', 1);
+%   model = arima('Constant', 0, 'MA', param_ma(:, i), 'Variance', 1); % innovation algorithm
 %   predict_ma(:, i) = simulate(model, n);
 % end
 
@@ -65,28 +65,28 @@ clc, clear;
 % error_ma=mean(mean((Y(1+K:n, :)-predict_ma(1+K:n, :)).^2))
 
 % c) Prediction on non-zero mean model.
-c = 10; % the constant
-model = arima('Constant', c, 'AR', {0.8}, 'Variance', 1);
+% c = 10; % the constant
+% model = arima('Constant', c, 'AR', {0.8}, 'Variance', 1);
 
-m = 50;
-n = 2000;
-Y = zeros(n, m);
-param_ar = zeros(3, m); % parameter, variance, mean
+% m = 50;
+% n = 500;
+% Y = zeros(n, m);
+% param_ar = zeros(3, m); % parameter, variance, mean
 
-for i=1:m
-  Y(:, i) = simulate(model, n);
-  param_ar(3, i) = mean(Y(:, i));
-  Y_norm(:, i) = Y(:, i)-mean(Y(:, i));
-  mle = arma_mle(Y_norm(:, i), 1, 0);
-  param_ar(1:2, i) = [mle.ar, mle.sigma^2];
-end
+% for i=1:m
+%   Y(:, i) = simulate(model, n);
+%   param_ar(3, i) = mean(Y(:, i));
+%   Y_norm(:, i) = Y(:, i)-mean(Y(:, i));
+%   mle = arma_mle(Y_norm(:, i), 1, 0);
+%   param_ar(1:2, i) = [mle.ar, mle.sigma^2];
+% end
 
-scatter(param_ar(1, :), param_ar(2, :));
+% scatter(param_ar(1, :), param_ar(2, :));
 
-predict_ar = zeros(n, m);
-K = 1;
-for i=1:m
-  predict_ar(:, i) = prediction(param_ar(1, i), Y(:, i), K, param_ar(3, i));
-end
+% predict_ar = zeros(n, m);
+% K = 1;
+% for i=1:m
+%   predict_ar(:, i) = prediction(param_ar(1, i), Y(:, i), K, param_ar(3, i));
+% end
 
-error_ar=mean(mean((Y(1+K:n, :)-predict_ar(1+K:n, :)).^2))
+% error_ar=mean(mean((Y(1+K:n, :)-predict_ar(1+K:n, :)).^2))
