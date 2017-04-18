d9a970acdb9d8a538db60a13b1b862a7a9db908e
Janet Jie Deng <janetjied@gmail.com>
2017-04-18 11:01:04 +0200

    minor fix for p1

--- p1.m
+++ p1.m
@@ -17,25 +17,25 @@ var = 1/n;
 h_1 = kstest(rho/sqrt(var));
 
 % (b) the portmanteau test (ljung-box q-test).
-h_2 = lbqtest(rho-mean(y));
+h_2 = lbqtest(rho-mean(rho));
 
 % Section 1.6 (a) the sample autocorrelation function
-h_3 = ACFbound(y, h);
+h_3 = ACFbound(rho, 40);
 
 % Section 1.6 (c) turning point test
-h_4 = turningPointTest(y);
+h_4 = turningPointTest(rho);
 
 % % 3. test with different models such as AR and MA.
-% ar = arima('Constant',0.05,'AR',{0.7},'Variance',0.01); % AR(3) model.
+% ar = arima('Constant',0.05,'MA',{0.7},'Variance',0.01); % AR(1) model.
 % y = simulate(ar, n);
-% rho = autocorr(y, h);
+% % rho = autocorr(y, h);
 % 
 % % (a) test the claim that p(h) has gaussian dist w/ mean 0 and variance 1/n.
-% var = 1/n;
-% h_1 = kstest(rho/sqrt(var));
+% var = 0.01;
+% h_1 = kstest(y/sqrt(var));
 % 
 % % (b) the portmanteau test (ljung-box q-test).
-% h_2 = lbqtest(rho-mean(y));
+% h_2 = lbqtest(y-mean(y));
 % 
 % % Section 1.6 (a) the sample autocorrelation function
 % h_3 = ACFbound(y, h);
@@ -43,7 +43,12 @@ h_4 = turningPointTest(y);
 % % Section 1.6 (c) turning point test
 % h_4 = turningPointTest(y);
 
-% figure(1)
+figure(1)
+x = 1:1:n;
+autocorr(rho, h)
+grid on;
+
+% figure(2)
 % x = 1:1:n;
 % plot(y)
 % title('White noise');
@@ -51,7 +56,3 @@ h_4 = turningPointTest(y);
 % ylabel('Sample Values');
 % grid on;
 
-figure(2)
-x = 1:1:n;
-autocorr(y, h)
-grid on;
--- p3.m
+++ p3.m
@@ -1,46 +1,47 @@
 % kth sf2943 tidsan17 (time series analysis) project problem 3 (prediction).
 
 clc, clear;
-% a) Predictions on \textbf{AR(2)} model.
+% % a) Predictions on \textbf{AR(2)} model.
 % model = arima('Constant', 0, 'AR', {1.3, -0.65}, 'Variance', 280);
-
+% 
 % m = 50;
 % n = 300;
-
+% c = 0;
+% 
 % Y = zeros(n, m);
 % param_2 = zeros(2, m);
 % param_10 = zeros(10, m); % AR(10)
-
+% 
 % for i=1:m
 %   Y(:, i) = simulate(model, n);
-
+% 
 %   mle = arma_mle(Y(:, i), 2, 0);
 %   param_2(:, i) = mle.ar;
-
+% 
 %   mle = arma_mle(Y(:, i), 10, 0);
 %   param_10(:, i) = mle.ar;
 % end
-
+% 
 % predict_2   = zeros(n, m);
 % predict_10  = zeros(n, m);
 % K = 3;
 % for i=1:m
-%   predict_2 (:, i) = prediction(param_2(:, i), Y(:, i), K);
-%   predict_10(:, i) = prediction(param_10(:, i), Y(:, i), K);
+%   predict_2 (:, i) = prediction(param_2(:, i), Y(:, i), K, c);
+%   predict_10(:, i) = prediction(param_10(:, i), Y(:, i), K, c);
 % end
-
+% 
 % error_2 =mean(mean((Y(1+K:n, :)-predict_2 (1+K:n, :)).^2))
 % error_10=mean(mean((Y(1+K:n, :)-predict_10(1+K:n, :)).^2))
-
+% 
 % % b) Prediction on \textbf{AR(1)} model.
 % model = arima('Constant', 0, 'AR', {0.8}, 'Variance', 1);
-
+% 
 % m = 50;
 % n = 2000;
 % Y = zeros(n, m);
 % param_ar = zeros(1, m); % AR(1)
 % param_ma = zeros(10, m); % MA(10)
-
+% 
 % for i=1:m
 %   Y(:, i) = simulate(model, n);
 %   mle = arma_mle(Y(:, i), 1, 0);
@@ -51,16 +52,16 @@ clc, clear;
 %     foo = foo*mle.ar;
 %   end
 % end
-
+% 
 % predict_ar = zeros(n, m);
 % predict_ma = zeros(n, m);
 % K = 3;
 % for i=1:m
-%   predict_ar(:, i) = prediction(param_ar(:, i), Y(:, i), K);
+%   predict_ar(:, i) = prediction(param_ar(:, i), Y(:, i), K, c);
 %   model = arima('Constant', 0, 'MA', param_ma(:, i), 'Variance', 1);
 %   predict_ma(:, i) = simulate(model, n);
 % end
-
+% 
 % error_ar=mean(mean((Y(1+K:n, :)-predict_ar(1+K:n, :)).^2))
 % error_ma=mean(mean((Y(1+K:n, :)-predict_ma(1+K:n, :)).^2))
 
