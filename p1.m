da46649e8fabd0e3f134356a6be3c49d535a9668
Janet Jie Deng <janetjied@gmail.com>
2017-05-05 02:10:25 +0200

    minor change for p6 and more graphs added.

--- p1.m
+++ p1.m
@@ -3,51 +3,57 @@
 clear;
 addpath ./src
 % initiate size of data and lag
-n = 500; % size n
+n = 1000; % size n
 h = 40; % lag
 
-% 1. generate iid ts datas.
-mu = 0;
-sigma = 1;
-y = sigma*randn(n, 1)+mu; % simulate iid samples
-[rho,lags,bounds] = autocorr(y, h);
-
-% 2. different tests.
-% (a) test the claim that p(h) has gaussian distribution with mean zero and variance 1/n.
-var = 1/n;
-h_1 = kstest(rho/sqrt(var));
-
-% (b) the portmanteau test (ljung-box q-test).
-h_2 = lbqtest(rho-mean(rho));
-
-% Section 1.6 (a) the sample autocorrelation function
-h_3 = ACFbound(rho, 40);
-
-% Section 1.6 (c) turning point test
-h_4 = turningPointTest(rho);
-
-% % 3. test with different models such as AR and MA.
-% ar = arima('Constant',0.05,'MA',{0.7},'Variance',0.01); % AR(1) model.
-% y = simulate(ar, n);
-% % rho = autocorr(y, h);
+% % 1. generate iid ts datas.
+% mu = 0;
+% sigma = 1;
+% y = sigma*randn(n, 1)+mu; % simulate iid samples
+% [rho,lags,bounds] = autocorr(y, h);
 % 
-% % (a) test the claim that p(h) has gaussian dist w/ mean 0 and variance 1/n.
-% var = 0.01;
-% h_1 = kstest(y/sqrt(var));
+% % 2. different tests.
+% % (a) test the claim that p(h) has gaussian distribution with mean zero and variance 1/n.
+% var = 1/n;
+% h_1 = kstest(rho/sqrt(var));
 % 
 % % (b) the portmanteau test (ljung-box q-test).
-% h_2 = lbqtest(y-mean(y));
+% h_2 = lbqtest(rho-mean(rho));
 % 
 % % Section 1.6 (a) the sample autocorrelation function
-% h_3 = ACFbound(y, h);
+% h_3 = ACFbound(rho, 40);
 % 
 % % Section 1.6 (c) turning point test
-% h_4 = turningPointTest(y);
+% h_4 = turningPointTest(rho);
+% 
+% 3. test with different models such as AR and MA.
+ar = arima('Constant',0.05,'MA',{0.7},'Variance',0.01); % AR(1) model.
+y = simulate(ar, n);
+rho = autocorr(y, h);
 
-figure(1)
-x = 1:1:n;
-autocorr(rho, h)
-grid on;
+% (a) test the claim that p(h) has gaussian dist w/ mean 0 and variance 1/n.
+var = 0.01;
+h_1 = kstest(y/sqrt(var));
+
+% (b) the portmanteau test (ljung-box q-test).
+h_2 = lbqtest(y-mean(y));
+
+% Section 1.6 (a) the sample autocorrelation function
+h_3 = ACFbound(y, h);
+
+% Section 1.6 (c) turning point test
+h_4 = turningPointTest(y);
+
+% figure(1)
+% x = 1:1:n;
+% autocorr(rho, h)
+% grid on;
+
+figure(2)
+% qqplot(rho/sqrt(var))
+% title('Q-Q plot for autocorreclations of iid samples')
+qqplot(y/sqrt(var))
+title('Q-Q plot for low-order AR/MR with samll coefficients')
 
 % figure(2)
 % x = 1:1:n;
--- p6.m
+++ p6.m
@@ -4,101 +4,110 @@
 % GARCH(2. 2) model fitted from 01/01/2017 to 03/05/2017.
 % we want to fit 95% confidence level for stock price of **04/05/2017**.
 clc, clear
-% 1.
-foo = [0.000034021 0.064860136 0.000000006 0.000817247 0.006258225];
-bar = [0.000000000 0.047206106];
-alpha_0 = foo(1);
-alpha = [foo(2) bar(1)];
-beta_ = [foo(3) bar(2)];
-mean_ = foo(4);
-std_ = foo(5);
-s_0 = 1633.99; % stock price of **03/05/2017**.
+% % 1.
+% foo = [0.000034021 0.064860136 0.000000006 0.000817247 0.006258225];
+% bar = [0.000000000 0.047206106];
+% alpha_0 = foo(1);
+% alpha = [foo(2) bar(1)];
+% beta_ = [foo(3) bar(2)];
+% mean_ = foo(4);
+% std_ = foo(5);
+% s_0 = 1633.99; % stock price of **03/05/2017**.
+% 
+% mu = 0;
+% z = [0.005830305 -0.001541706]; % log-reutrns distribution
+% v = [-0.001926 -0.002670];
+% var_ = alpha_0+alpha*z'+beta_*v'; % 2.8614e-04
+% 
+% num = 100;
+% s_k = zeros(num, 1);
+% for i=1:num
+%   x_t = randn()*sqrt(var_);
+%   y_t = mu+x_t;
+%   s_k(i) = s_0*exp(y_t); % 1.6870e+03
+% end
+% 
+% var_ = var(s_k);
+% q_95_up_garch = mean(s_k)+1.96*sqrt(var_);
+% q_95_down_garch = mean(s_k)-1.96*sqrt(var_);
+% 
+% 
+% % naive approach.
+% s_k = zeros(num, 1);
+% for i=1:num
+%   s_k(i) = s_0*exp(sqrt(abs(v(1)))*randn());
+% end
+% var_ = var(s_k);
+% q_95_up_naive = mean(s_k)+1.96*sqrt(var_); % 1.7640e+03
+% q_95_down_naive = mean(s_k)-1.96*sqrt(var_);
 
-mu = 0;
-z = [0.005830305 -0.001541706];
-v = [-0.001926 -0.002670];
-var_ = alpha_0+alpha*z'+beta_*v'; % 2.8614e-04
+% % 2.
+% % 2010-08-25 is 250 days before 2011-08-18.
+% % using GARCH(2, 2).
+% clc, clear
+% alpha_0 = 0.0000009838;
+% alpha = [0.043909004 0.191871594];
+% beta_ = [0.032837324 0.686450789];
+% mean_ = -0.00043583;
+% std_ = 0.01251815;
+% s_0 = 893.0758; % too small...
+% 
+% v = zeros(12, 1);
+% x = zeros(12, 1);
+% v(1:2) = [-0.07 0]; 
+% x(1:2) = [-0.069248 -0.0692479];
+% num = 100;
+% s_k = zeros(num, 1);
+% 
+% for j=1:num
+%   for i=1:10
+%     var_ = alpha_0+alpha*v(i:i+1)+beta_*x(i:i+1);
+%     x_t = randn()*sqrt(abs(var_));
+% 
+%     v(i+2) = var_;
+%     x(i+2) = x_t;
+%   end
+%   sum_x = sum(x(3:12));
+%   exp_x = exp(sum_x);
+%   s_k(j) = s_0*exp(mean(x(3:12)));
+% end
+% 
+% var_ = var(s_k);
+% mmean = mean(s_k);
+% q_05_down = mean(s_k)-1.96*sqrt(var_); % -1.7703e+04
+% q_05_up = mean(s_k)+1.96*sqrt(var_);
 
-num = 100;
-s_k = zeros(num, 1);
-for i=1:num
-  x_t = randn()*sqrt(var_);
-  y_t = mu+x_t;
-  s_k(i) = s_0*exp(y_t); % 1.6870e+03
-end
 
-var_ = var(s_k);
-q_95 = mean(s_k)+1.96*sqrt(var_)
-
-% naive approach.
-s_k = zeros(num, 1);
-for i=1:num
-  s_k(i) = s_0*exp(sqrt(abs(v(1)))*randn());
-end
-var_ = var(s_k);
-q_95 = mean(s_k)+1.96*sqrt(var_) % 1.7640e+03
-
-% 2.
-% 2010-08-25 is 250 days before 2011-08-18.
-% using GARCH(2, 2).
-clc, clear
-alpha_0 = 0.0000009838;
-alpha = [0.043909004 0.191871594];
-beta_ = [0.032837324 0.686450789];
-mean_ = -0.00043583;
-std_ = 0.01251815;
-s_0 = 893.0758; % too small...
-
-v = zeros(12, 1);
-x = zeros(12, 1);
-v(1:2) = [-0.07 0]; 
-x(1:2) = [-0.069248 -0.0692479];
-num = 100;
-s_k = zeros(num, 1);
-
-for j=1:num
-  for i=1:10
-    var_ = alpha_0+alpha*v(i:i+1)+beta_*x(i:i+1);
-    x_t = randn()*sqrt(abs(var_));
-
-    v(i+2) = var_;
-    x(i+2) = x_t;
-  end
-  s_k(j) = s_0*exp(sum(x(3:12)));
-end
-
-var_ = var(s_k);
-q_05 = mean(s_k)-1.96*sqrt(var_) % -1.7703e+04
-
-% 2013-04-25 is 250 days before 2014-04-24.
-clc, clear
-alpha_0 = 0.000025131;
-alpha = [0.103642507 0];
-beta_ = [0.011588480 0.520348628];
-mean_ = 0.00053760;
-std_ = 0.00834714;
-s_0 = 1365.266650; % too small...
-
-v = zeros(12, 1);
-x = zeros(12, 1);
-v(1:2) = [0.008443 0.0084427]; 
-x(1:2) = [0.01 -0.01];
-num = 100;
-s_k = zeros(num, 1);
-
-for j=1:num
-  for i=1:10
-    var_ = alpha_0+alpha*v(i:i+1)+beta_*x(i:i+1);
-    x_t = randn()*sqrt(abs(var_));
-
-    v(i+2) = var_;
-    x(i+2) = x_t;
-  end
-  s_k(j) = s_0*exp(sum(x(3:12)));
-end
-
-var_ = var(s_k); % mean = 1.8356e+03
-q_05 = mean(s_k)-1.96*sqrt(var_) % -1.1036e+03
+% % 2013-04-25 is 250 days before 2014-04-24.
+% clc, clear
+% alpha_0 = 0.000025131;
+% alpha = [0.103642507 0];
+% beta_ = [0.011588480 0.520348628];
+% mean_ = 0.00053760;
+% std_ = 0.00834714;
+% s_0 = 1365.266650; % too small...
+% 
+% v = zeros(12, 1);
+% x = zeros(12, 1);
+% v(1:2) = [0.008443 0.0084427]; 
+% x(1:2) = [0.01 -0.01];
+% num = 100;
+% s_k = zeros(num, 1);
+% 
+% for j=1:num
+%   for i=1:10
+%     var_ = alpha_0+alpha*v(i:i+1)+beta_*x(i:i+1);
+%     x_t = randn()*sqrt(abs(var_));
+% 
+%     v(i+2) = var_;
+%     x(i+2) = x_t;
+%   end
+%   s_k(j) = s_0*exp(mean(x(3:12)));
+% end
+% 
+% var_ = var(s_k); % mean = 1.8356e+03
+% q_05_down = mean(s_k)-1.96*sqrt(var_); % -1.1036e+03
+% q_05_up = mean(s_k)+1.96*sqrt(var_);
 
 % 20 June 2014 (2014-06-20) is 250 business days before 2015-06-18.
 clc, clear
@@ -122,8 +131,9 @@ for j=1:num
     v(i+2) = var_;
     x(i+2) = x_t;
   end
-  s_k(j) = s_0*exp(sum(x(3:12)));
+  s_k(j) = s_0*exp(mean(x(3:12)));
 end
 
 var_ = var(s_k); % mean = 2.1933e+03
-q_05 = mean(s_k)-1.96*sqrt(var_) % -3.5407e+03
+q_05_down = mean(s_k)-1.96*sqrt(var_); % -3.5407e+03
+q_05_up = mean(s_k)+1.96*sqrt(var_);
